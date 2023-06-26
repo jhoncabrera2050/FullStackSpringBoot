15b7cb884b7f8795b91ad6488ae1c94b9b854e63
jhoncabrera <jhoncabrera2050@gmail.com>
2023-06-26 14:58:21 -0500

    inicio del proyecto

---
+++ src/test/java/com/ventas/ventas/VentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ventas.ventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
